d0d1c1c52d02b49ef4a893a6452686de8e437bca
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-25 23:06:54 +0600

    solved task 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//Task 1: You have an odd array. now convert this array into an even array.(using for loop & map method)
+//Task 1:
+// You have an odd array. now convert this array into an even array.(using for loop & map method)
 
 // Using for loop
 const oddNumbers = [1, 3, 5, 7, 9];
@@ -9,9 +10,21 @@ function makeEven(){
     return oddNumbers;
 }
 console.log(makeEven());
+
 //Using map
 const oddNumbers2 = [3, 1, 11, 13, 9];
 const evenNumbers = oddNumbers2.map(n => n+1);
 console.log(evenNumbers);
 
-//Task 2: 
+//Task 2:
+// You are given an array say: [33, 50, 79, 78, 90, 101, 30]. Now return/get all the elements
+// which are divisible by 10 using array.filter() method. Then again do the same task using array.find() method.
+
+//Using filter method
+const numbers = [33, 50, 79, 78, 90, 101, 30];
+const divisibleByTen = numbers.filter(n => n % 10===0)
+console.log(divisibleByTen);
+
+// using find method
+const divisibleByTen2 = numbers.find(n => n % 10===0)
+console.log(divisibleByTen2);
